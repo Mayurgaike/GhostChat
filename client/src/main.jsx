b0be5e90a60e08a71011ffc3e86402ce7a28f90b
Mayurgaike <mayurgaike486@gmail.com>
2025-04-19 13:18:15 +0530

    Updated theme

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,11 +4,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
